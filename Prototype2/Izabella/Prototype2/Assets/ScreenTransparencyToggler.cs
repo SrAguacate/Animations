164d156c146296464921caff828d1812211f49ae
iszen <szenbella@gmail.com>
2016-11-22 08:09:15 +0100

    added prefinal version of interactable room

---
+++ Prototype2/Izabella/Prototype2/Assets/ScreenTransparencyToggler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace NewtonVR
+{
+    public class ScreenTransparencyToggler : MonoBehaviour
+    {
+
+        public NVRButton Button;
+
+        public GameObject screen;
+
+        private void Update()
+        {
+            if (Button.ButtonDown)
+            {
+                screen.GetComponent<Renderer>().enabled = false;
+            }
+        }
+    }
+}
